c06367bbd26775b4bf5a14dfdd1150b64bd3ef6a
BrijeshBumrela <37417813+BrijeshBumrela@users.noreply.github.com>
2018-10-10 16:04:56 +0530

    added insertion

--- flask/app.py
+++ flask/app.py
@@ -29,7 +29,22 @@ for i in num:
 	F.write(",".join(i) + "\n")
 F.close()
 
-
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+	
 
 num2 = get_TT_data2()
 num2 = [i.split(',') for i in num2]
